8ce905e9f1b235d8e09ae8afaba9d13d6e3ee7ae
sherief abdelmageed <sheriefabdelmageed@gmail.com>
2020-04-05 20:50:38 +0200

    prod json

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter history={HashRouter}>
     <App />
   </BrowserRouter>,
   document.getElementById("root")
